b1b8379ac281771227f8fc128e1e7eb45cfc0ec0
EZEN-139 <EZEN-139@DESKTOP-JB65I3M>
2022-07-13 15:58:22 +0900

    lyb

---
+++ lyb/src/lyb/lyb.java
@@ -0,0 +1,19 @@
+package lyb;
+
+public class lyb {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("PSU " + "GH " + "GMJ ");
+		System.out.println("KSJ " + "KJS " + "KTH ");
+		System.out.println("NGH " + "DH " + "PYM ");
+		System.out.println("SDY " + "YBG " + "YJ ");
+		System.out.println("LJH " + "JYS " + "JHS ");
+		
+		
+		
+	}
+
+}
